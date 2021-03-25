1f813dc343b294fb226ef384fd1a75529486067f
Anushan Santhirakumar <anushan1508@gmail.com>
2021-03-25 15:30:44 +0530

    mongo

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection')
+
 const app = express();
 
 dotenv.config({path:'config.env'})
@@ -11,6 +13,9 @@ const PORT = process.env.PORT || 8000;
 
 // Log Requests
 app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
 
 // Parse request to body-parser
 app.use(bodyParser.urlencoded({extended:true}))
@@ -24,9 +29,8 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+// Load Routers
+app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
